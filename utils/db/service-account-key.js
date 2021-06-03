58e92d4395b44bfe221d4e6845aa00c40f56787d
Tom Howland <tomhowland9@gmail.com>
2021-06-03 18:09:58 +0100

    see env again again

--- utils/db/service-account-key.js
+++ utils/db/service-account-key.js
@@ -1,10 +1,8 @@
-console.log(process.env);
-
 export const serviceAccountKey = {
   "type": "service_account",
   "project_id": "found-my",
   "private_key_id": "d29c8fe5ce8203807d29923a27af0fed8c80ea07",
-  "private_key": process.env.DB_KEY,
+  "private_key": process.env.SERVER_DB_KEY,
   "client_email": "firebase-adminsdk-sj593@found-my.iam.gserviceaccount.com",
   "client_id": "100465128052160661242",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -12,4 +10,3 @@ export const serviceAccountKey = {
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sj593%40found-my.iam.gserviceaccount.com"
 };
-console.log(serviceAccountKey);

325fae72188aa499fb5fcb0517779186ca563302
TomHowland <tom.howland@boomin.com>
2021-09-18 21:42:37 +0100

    Fix build error

--- pages/[userHandle]/[itemHandle].tsx
+++ pages/[userHandle]/[itemHandle].tsx
@@ -13,41 +13,7 @@ type ServerSideProps = {
 
 export default function ItemPage({ user, item }: ServerSideProps) {
   return (
-    // <div className={styles.page}>
-    //   <header className={styles.header}>
-    //     <p>
-    //       Hi, I'm <mark>{user.userFullName}</mark> and you have found my{' '}
-    //       <mark>{item.itemName}</mark>
-    //     </p>
-    //   </header>
-    //   <main className={styles.main}>
-    //     <div className={styles.cta}>
-    //       I would appreciate it if you could get in touch, and I'd be happy to
-    //       come and get it
-    //     </div>
-    //     <a
-    //       className={styles.contactMethod}
-    //       href={'mailto:' + user.userEmailAddress}
-    //     >
-    //       <FontAwesomeIcon icon={faEnvelope} />
-    //       <span className={styles.a}>By email</span>
-    //     </a>
-    //     <a
-    //       className={styles.contactMethod}
-    //       href={'sms:' + user.userPhoneNumber}
-    //     >
-    //       <FontAwesomeIcon icon={faComments} />
-    //       <span className={styles.a}>By text</span>
-    //     </a>
-    //     <a
-    //       className={styles.contactMethod}
-    //       href={'https://wa.me/' + user.userPhoneNumber}
-    //     >
-    //       <FontAwesomeIcon icon={faWhatsapp} />
-    //       <span className={styles.a}>By WhatsApp</span>
-    //     </a>
-    //   </main>
-    // </div>
+    <></>
   );
 }
 

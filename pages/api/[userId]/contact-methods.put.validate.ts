8546142484001722ea3659f42f210f68850d022d
TomHowland <tom.howland@boomin.com>
2021-09-19 10:36:38 +0100

    Fix build error

--- pages/api/[userId]/contact-methods.put.validate.ts
+++ pages/api/[userId]/contact-methods.put.validate.ts
@@ -1,6 +1,3 @@
-import { IUpdateContactMethodsRequest } from 'models/api/update-contact-methods';
 import { ApiPutRequest } from 'utils/api/api-request';
 
-export async function validate(
-  req: ApiPutRequest<IUpdateContactMethodsRequest>
-) {}
+export async function validate(req: ApiPutRequest<any>) {}

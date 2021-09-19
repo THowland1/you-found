7a6562c4283617d9d0ee18552c2ef707d07c0dbe
TomHowland <tom.howland@boomin.com>
2021-09-19 10:52:24 +0100

    Fix build error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    target: 'serverless',
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"]
-        });
+  target: 'experimental-serverless-trace',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
 
-        return config;
-    }
-}
+    return config;
+  }
+};
